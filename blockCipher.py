36ffee9f76a2cad19be553384d28d7dfb51e300d
Aya-gamal2211 <ayaahmadgamal2000@gmail.com>
2024-04-19 20:18:02 +0200

    Update blockCipher.py

--- blockCipher.py
+++ blockCipher.py
@@ -1,3 +1,12 @@
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 
+def encrypt(plainText):
+    
+    key = get_random_bytes(16)
+    #CBC Mode: It is a mode of operation where each plaintext block gets XOR-ed 
+    # with the previous ciphertext block prior to encryption.
+    cipher = AES.new(key, AES.MODE_CBC)
+    
+    
+
