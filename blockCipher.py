59a394690efcc4827f7ad4bb01773165bca4a820
Aya-gamal2211 <ayaahmadgamal2000@gmail.com>
2024-04-19 23:16:52 +0200

    Update blockCipher.py

--- blockCipher.py
+++ blockCipher.py
@@ -2,6 +2,7 @@ from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 from Crypto.Util.Padding import pad
 from secrets import token_bytes
+from Crypto.PublicKey import RSA
 
 
 
@@ -11,7 +12,8 @@ def encrypt(plainText):
     iv = token_bytes(AES.block_size)
     #CBC Mode: It is a mode of operation where each plaintext block gets XOR-ed 
     # with the previous ciphertext block prior to encryption.
-    cipher = AES.new(key, AES.MODE_CBC,iv)
+    # cipher = AES.new(key, AES.MODE_CBC,iv)
+    cipher = AES.new(key, AES.MODE_EAX)
     plainTextPadded=pad(plainText.encode(), AES.block_size)
     ct_bytes = cipher.encrypt(plainTextPadded)
 
@@ -21,6 +23,13 @@ def encrypt(plainText):
 
 encrypted=encrypt("19p1689")
 print("The encrypted Text is: ", encrypted)
+
+def hashingModule():
+
+    
+def RSAKey():
+    
+    return
 
     
 
