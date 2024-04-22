ad6ef81fd21576cc150a35887b2f6cbbac68a4b8
Aya-gamal2211 <ayaahmadgamal2000@gmail.com>
2024-04-22 20:28:44 +0200

    Examples of RSA , ECC

--- blockCipher.py
+++ blockCipher.py
@@ -61,7 +61,15 @@ def ECCKey():
 )
     public_key = private_key.public_key()
     return private_key,public_key
-    
 
+
+    
+rsa_private_key, rsa_public_key = RSAKey()
+print("RSA Private key:", rsa_private_key)
+print("RSA Public key:", rsa_public_key)
+
+ecc_private_key, ecc_public_key = ECCKey()
+print("ECC Private key:", ecc_private_key)
+print("ECC Public key:", ecc_public_key)
     
 
