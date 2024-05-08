7e4637222c0b170ee5abe340a0e402be6794a2ff
Shaimaa-moh <93445956+Shaimaa-moh@users.noreply.github.com>
2024-05-08 15:56:19 +0300

    added some code fixes
    signing the msg with the private key and make hashing , created different signature file for each user instead of overriding on the same file , signature is unique for each user going to send even if it is the same message

--- KeyManagement/RSA/Authentication/Verify_signature.py
+++ KeyManagement/RSA/Authentication/Verify_signature.py
@@ -1,5 +1,5 @@
 import rsa
-############# verify the user at decrypting the message ###############
+############# verify the receiver at decrypting the message ###############
 
 username=input("Enter your username : ")
 with open (f"{username}_public.pem") as f :
@@ -14,12 +14,13 @@ message=open ('message','rb').read()
 signature=open ('signature_file','rb').read()
 
 try:
+    
+    rsa.verify(message,signature,public_key)
+    print("signature is correct , success sending message")
     encrypted_msg =open ('encrypted_msg1','rb').read()
+    ## check the original plain text
     plaintext = rsa.decrypt(encrypted_msg,private_key) 
     print("plain text is : ",plaintext)
-    rsa.verify(encrypted_msg,signature,public_key)
-    print("signature is correct , success sending message")
-
 except:
     print("your signature is incorrect, check it again")
 
--- KeyManagement/RSA/Authentication/authenticate.py
+++ KeyManagement/RSA/Authentication/authenticate.py
@@ -27,7 +27,6 @@ def register_new(username, password):
     except Exception as e:
         print("Error:", e)
 
-# print(verify("shimo", "test1", "auth.txt"))
 def generate_keys(username):
     private_key_file = f"{username}_private.pem"
     public_key_file = f"{username}_public.pem"
@@ -40,15 +39,14 @@ def generate_keys(username):
         with open(public_key_file, "wb") as f:
             f.write(publicKey.save_pkcs1("PEM"))
 
-# check that the user is logged in
+# check that the user is logged in then he will be able to send a message
 def authentication(): 
     choice = input('Enter 1 to signIn or 2 to register \n')
     if choice == '1':
         username = input("Please enter your username :")
-        password = input("Enter your password: ")
+        password = input("Enter your password : ")
         isUser = SignIn(username, password, "auth.txt")
         generate_keys(username)  # Generate keys if they don't exist
-
         if isUser:
             with open (f"{username}_private.pem","rb") as f :
                 private_key = rsa.PrivateKey.load_pkcs1(f.read())
@@ -58,22 +56,26 @@ def authentication():
 
             ############# sign the message with the owners of the private key ########
             message = open('message','rb').read()
+            # hash the original message using SHA-256 algorithm
+            #hash_value = rsa.compute_hash(message, "SHA-256")
+            # add the message , private key for each user and the hashed value in the signature
+            signature = rsa.sign(message, private_key, "SHA-256")
+            with open(f"{username}_signature",'wb') as signature_file:
+                signature_file.write(signature)
+                signature_file.close()
+            print("Your signature on the message is successfully created  ") 
+            ## if we want to encrypt the message to check its validity alone
             encrypted_msg = rsa.encrypt(message, public_key)
             print("The encrypted message is", encrypted_msg)
             # Write the encrypted message to a file
             with open('encrypted_msg1','wb') as f:
                 f.write(encrypted_msg)
                 f.close()
-            hash_value = rsa.compute_hash(encrypted_msg, "SHA-256")
-            signature = rsa.sign(encrypted_msg, private_key, "SHA-256")
-            with open('signature_file','wb') as signature_file:
-                signature_file.write(signature)
-            print("Your signature on the message is successfully created  ")
+
         else:
             print("Wrong username or password")
 
     elif choice == '2':
-        #f.truncate()
         username = input("Enter your username: ")
         password = input("Enter your password: ")
         register_new(username, password)
